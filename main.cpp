62b61e80a09c47e7ec22e79ab47c806a5b895a89
sofiamenendez <samenendez97@unitec.edu>
2016-02-20 16:31:31 -0600

    Added files via upload

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+//ingresar un numero entero y finalizar cuando el numero sea negativo//
+
+int main()
+{   int numero;
+   do
+   { cout<<"ingresar el numero>";
+   cin>>numero;
+
+   }while (numero>0);
+    return 0;
+}
